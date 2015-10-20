f143cce434a0a5a9d1bc49c03732245ed1a5fbd7
CoderEven <huangjinwen@JinwendeMacBook-Pro.local>
2015-10-20 10:41:41 +0800

    修改Demo注释
    删除掉多余的注释

--- JWStatusHUDDemo/JWStatusHUDDemo/ViewController.m
+++ JWStatusHUDDemo/JWStatusHUDDemo/ViewController.m
@@ -25,11 +25,6 @@
 
 - (IBAction)loading {
     [JWStatusHUD showLoading:@"数据加载中..."];
-    
-//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-//        
-//        [JWStatusHUD hide];
-//    });
 }
 
 - (IBAction)hide {
